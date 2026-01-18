3cf19cca9a505b2002bfb85d175bd065db420562
Jin Won Lee <jinwonlee0916@gmail.com>
2026-01-18 02:04:25 -0500

    new update - thumb_down

--- hand_gesture/main.py
+++ hand_gesture/main.py
@@ -113,11 +113,11 @@ def run_gesture_logic():
                 if engine.latest_result and engine.latest_result.gestures:
                     # Get the top gesture (highest score)
                     top_gesture = engine.latest_result.gestures[0][0].category_name
-                    
                     if top_gesture == "ILoveYou":
                         voice_assistant.run()
                         engine.latest_result = None
-                        pass
+                    elif top_gesture == "Thumb_Down":
+                        os._exit(0)
                 fingers_up = engine.count_fingers_up(hand1)
 
                 # Key points
