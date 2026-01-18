e0ef85878caeedeac2b36b0e1ea749ea5bcee173
Tyler Li <jingge.li@mail.mcgill.ca>
2026-01-18 08:11:40 +0800

    last go commit?

--- go/main.go
+++ go/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"mchack13.com/go/handlers"
+	"mchack13.com/go/middleware"
 )
 
 func main() {
@@ -19,15 +27,19 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
-	// Set up gRPC connection to python
-	//conn, err := grpc.NewClient(pythonPort, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	//if err != nil {
-	//	logger.Error("did not connect: %v", err)
-	//}
-	//defer conn.Close()
+	//Set up gRPC connection to python
+	conn, err := grpc.NewClient(pythonPort, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		logger.Error("did not connect: %v", err)
+	}
+	defer conn.Close()
 
 	// http server
-	//mux := http.NewServeMux()
+	mux := http.NewServeMux()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
 	// websocket handlers
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
@@ -35,42 +47,38 @@ func main() {
 	})
 
 	slog.Info("Server starting on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		slog.Error("Server failed", "error", err)
-		os.Exit(1)
-	}
 
 	// handlers
-	//mux.HandleFunc("GET /health", handlers.Health)
+	mux.HandleFunc("GET /health", handlers.Health)
 	//mux.HandleFunc("GET /api/v1/users/{id}", handlers.GetUser)
 	//mux.HandleFunc("POST /api/v1/users", handlers.CreateUser)
 
 	// Create the Server
 	// Wrap mux with the middleware from the 'middleware' package
-	//srv := &http.Server{
-	//	Addr:         ":" + port,
-	//	Handler:      middleware.Logging(mux),
-	//	ReadTimeout:  5 * time.Second,
-	//	WriteTimeout: 10 * time.Second,
-	//	IdleTimeout:  120 * time.Second,
-	//}
+	srv := &http.Server{
+		Addr:         ":" + port,
+		Handler:      middleware.Logging(mux),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
 	//
 	//// Graceful Shutdown Context
-	//ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	//defer stop()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	//
 	//// Start Server
-	//go func() {
-	//	logger.Info("Starting server", "port", port, "go_version", "1.25")
-	//	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-	//		logger.Error("Could not start server", "error", err)
-	//		os.Exit(1)
-	//	}
-	//}()
+	go func() {
+		logger.Info("Starting server", "port", port, "go_version", "1.25")
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("Could not start server", "error", err)
+			os.Exit(1)
+		}
+	}()
 	//
 	//// Wait for Signal
-	//<-ctx.Done()
-	//logger.Info("Shutting down gracefully...")
+	<-ctx.Done()
+	logger.Info("Shutting down gracefully...")
 	//
 	////  Shutdown with Timeout
 	//shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
